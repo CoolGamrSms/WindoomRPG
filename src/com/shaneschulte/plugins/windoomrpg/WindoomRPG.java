9bef7bcf33a613b41b46f0411e501c2af1f511e5
Shane Schulte <schulte.shane@gmail.com>
2015-05-09 06:31:34 -0400

    Initialize Repository

---
+++ src/com/shaneschulte/plugins/windoomrpg/WindoomRPG.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shaneschulte.plugins.windoomrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Shane
+ */
+public class WindoomRPG extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+    @Override
+    public void onDisable() {
+        
+    }
+}
